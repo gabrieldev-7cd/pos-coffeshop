31a66a02045c537b3d977ab2c3a6ec5fa8a699ae
gabrieldev-7cd <gabriel.alexandre.dev@gmail.com>
2022-02-16 22:54:35 -0300

    Optinional buy card

--- coffe/src/views/Home.vue
+++ coffe/src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app style="{background: $vuetify.theme.themes.dark.brackround}">
+  <v-app :style="{background: $vuetify.theme.themes.dark.background}">
     <sideBar/>
     <sideBarRight/>
     <v-container>
@@ -179,6 +179,228 @@
         <v-spacer/>
         <span color="grey">12 Coffees results</span>
       </v-toolbar>
+      <v-row>
+        <v-col cols="12" sm="6">
+          <v-card flat class="rounded-lg mx-5">
+            <v-list-item three-line>
+              <v-list-item-avatar rounded size="120" color="grey lighten-4">
+                <v-img src="2.png"/>
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title class="text-h5">
+                  Caramel Frappuccino
+                </v-list-item-title>
+                <v-list-item-subtitle class="mt-1">
+                  Caramel sytrup with coffe, milk and whipped cream
+                </v-list-item-subtitle>
+                <strong class="mt-3">
+                  $3.95
+                </strong>
+              </v-list-item-content>
+            </v-list-item>
+            <v-card-actions>
+              <v-row>
+                <v-col cols="12" sm="6" class="mt-1">
+                  <strong class="ml-3">Mood</strong><br>
+                  <v-item-group mandatory class="mt-n1">
+                    <v-container>
+                      <v-row>
+                        <!-- Mood's -->
+                        <v-col cols="12" md="5">
+                          <v-item v-slot="{active, toggle}">
+                            <v-card 
+                              color="#F6EFEF"
+                              :class="active ? 'borderme' : '' "
+                              class="d-flex align-center rounded-circle"
+                              dark  width="50" height="50" @click="toggle" flat
+                            >
+                              <v-list-item class="text-center px-2">
+                                <v-list-item-content>
+                                  <v-icon dark color="orange">fas fa-fire</v-icon>
+                                </v-list-item-content>
+                              </v-list-item>
+                            </v-card>
+                          </v-item>
+                        </v-col>
+
+                        <v-col cols="12" md="6">
+                          <v-item v-slot="{active, toggle}">
+                            <v-card 
+                              color="#F6EFEF"
+                              :class="active ? 'borderme' : '' "
+                              class="d-flex align-center rounded-circle"
+                              dark  width="50" height="50" @click="toggle" flat
+                            >
+                              <v-list-item class="text-center px-2">
+                                <v-list-item-content>
+                                  <v-icon dark color="blue">fab fa-modx</v-icon>
+                                </v-list-item-content>
+                              </v-list-item>
+                            </v-card>
+                          </v-item>
+                        </v-col>
+
+                      </v-row>
+                    </v-container>
+                  </v-item-group>
+                </v-col>
+
+                <!-- Size's -->
+                <v-col cols="12" sm="6" class="mt-1">
+                  <strong class="ml-2">Size</strong>
+                  <v-item-group mandatory class="mt-n1">
+                    <v-container>
+                      <v-row>
+                        <v-col cols="12" sm="4">
+                          <v-item v-slot="{active, toggle}">
+                            <v-card color="#F6EFEF" :class="active ? 'borderme' : '' "
+                              class="d-flex align-center rounded-circle"
+                              height="50" width="50" @click="toggle" flat
+                            >
+                              <v-list-item class="text-center">
+                                <strong class="font-weight-black">S</strong>
+                              </v-list-item>
+                            </v-card>
+                          </v-item>
+                        </v-col>
+
+                        <v-col cols="12" sm="4">
+                          <v-item v-slot="{active, toggle}">
+                            <v-card color="#F6EFEF" :class="active ? 'borderme' : '' "
+                              class="d-flex align-center rounded-circle"
+                              height="50" width="50" @click="toggle" flat
+                            >
+                             <v-list-item class="text-center">
+                                <strong class="font-weight-black">M</strong>
+                              </v-list-item>
+                            </v-card>
+                          </v-item>
+                        </v-col>
+
+                         <v-col cols="12" sm="4">
+                          <v-item v-slot="{active, toggle}">
+                            <v-card color="#F6EFEF" :class="active ? 'borderme' : '' "
+                              class="d-flex align-center rounded-circle"
+                              height="50" width="50" @click="toggle" flat
+                            >
+                             <v-list-item class="text-center">
+                                <strong class="font-weight-black">L</strong>
+                              </v-list-item>
+                            </v-card>
+                          </v-item>
+                        </v-col>
+
+                      </v-row>
+                    </v-container>
+                 </v-item-group>
+
+                </v-col>
+              </v-row>
+            </v-card-actions>
+            
+             <v-card-actions>
+              <v-row>
+                <v-col cols="12" sm="6" class="mt-1">
+                  <strong class="ml-3">Sugar</strong><br>
+                  <v-item-group mandatory class="mt-n1">
+                    <v-container>
+                      <v-row>
+                        <!-- Mood's -->
+                        <v-col cols="12" md="5">
+                          <v-item v-slot="{active, toggle}">
+                            <v-card 
+                              color="#F6EFEF"
+                              :class="active ? 'borderme' : '' "
+                              class="d-flex align-center rounded-circle"
+                              dark  width="50" height="50" @click="toggle" flat
+                            >
+                              <v-list-item class="text-center px-2">
+                                <v-list-item-content>
+                                  <strong class="overline" :class="active ? 'brown--text' : 'red--text' ">30%</strong>
+                                </v-list-item-content>
+                              </v-list-item>
+                            </v-card>
+                          </v-item>
+                        </v-col>
+
+                        <v-col cols="12" md="6">
+                          <v-item v-slot="{active, toggle}">
+                            <v-card 
+                              color="#F6EFEF"
+                              :class="active ? 'borderme' : '' "
+                              class="d-flex align-center rounded-circle"
+                              dark  width="50" height="50" @click="toggle" flat
+                            >
+                              <v-list-item class="text-center px-2">
+                                <v-list-item-content>
+                                  <v-icon dark color="blue">fab fa-modx</v-icon>
+                                </v-list-item-content>
+                              </v-list-item>
+                            </v-card>
+                          </v-item>
+                        </v-col>
+
+                      </v-row>
+                    </v-container>
+                  </v-item-group>
+                </v-col>
+
+                <!-- Size's -->
+                <v-col cols="12" sm="6" class="mt-1">
+                  <strong class="ml-2">Size</strong>
+                  <v-item-group mandatory class="mt-n1">
+                    <v-container>
+                      <v-row>
+                        <v-col cols="12" sm="4">
+                          <v-item v-slot="{active, toggle}">
+                            <v-card color="#F6EFEF" :class="active ? 'borderme' : '' "
+                              class="d-flex align-center rounded-circle"
+                              height="50" width="50" @click="toggle" flat
+                            >
+                              <v-list-item class="text-center">
+                                <strong class="font-weight-black">S</strong>
+                              </v-list-item>
+                            </v-card>
+                          </v-item>
+                        </v-col>
+
+                        <v-col cols="12" sm="4">
+                          <v-item v-slot="{active, toggle}">
+                            <v-card color="#F6EFEF" :class="active ? 'borderme' : '' "
+                              class="d-flex align-center rounded-circle"
+                              height="50" width="50" @click="toggle" flat
+                            >
+                             <v-list-item class="text-center">
+                                <strong class="font-weight-black">M</strong>
+                              </v-list-item>
+                            </v-card>
+                          </v-item>
+                        </v-col>
+
+                         <v-col cols="12" sm="4">
+                          <v-item v-slot="{active, toggle}">
+                            <v-card color="#F6EFEF" :class="active ? 'borderme' : '' "
+                              class="d-flex align-center rounded-circle"
+                              height="50" width="50" @click="toggle" flat
+                            >
+                             <v-list-item class="text-center">
+                                <strong class="font-weight-black">L</strong>
+                              </v-list-item>
+                            </v-card>
+                          </v-item>
+                        </v-col>
+
+                      </v-row>
+                    </v-container>
+                 </v-item-group>
+
+                </v-col>
+              </v-row>
+            </v-card-actions>
+        
+          </v-card>
+        </v-col>
+      </v-row>
     </v-container>
   </v-app>
 </template>
@@ -187,17 +409,19 @@
   import SideBar from '../components/SideBar'
   import SideBarRight from '../components/SideBarRight'
 
-  export default {
+  export default 
+  {
     name: 'Home',
 
-    components: {
+    components: 
+    {
       SideBar,
       SideBarRight
     }
   }
 </script>
 <style>
-  .v-card-borderme
+  .v-card.borderme
   {
     border: 2px solid #704232 !important;
   }
